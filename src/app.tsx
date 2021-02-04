6ef00df9305671ed3a8bcd107a170abfc5683962
Ulrich Fischer <careforthegoodones@gmail.com>
2021-02-04 19:48:23 +0100

    Unify indentation

--- src/app.tsx
+++ src/app.tsx
@@ -4,60 +4,60 @@ import './styles/cistercian.scss'
 
 export const App = () => {
 
-	const [value, setValue] = useState('2021');
+   const [value, setValue] = useState('2021');
 
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 
-		var value = event.target.value;
-		setValue(value ? value.replace(/\D/g, '') : '');
-	}
+      var value = event.target.value;
+      setValue(value ? value.replace(/\D/g, '') : '');
+   }
 
-	const shift = (coords: [number, number][], rt: boolean, offset: number) => {
+   const shift = (coords: [number, number][], rt: boolean, offset: number) => {
 
-		return coords.map(a => {
+      return coords.map(a => {
 
-			let x = rt ? a[0] * -1 : a[0];
-			let y = a[1] + offset;
+         let x = rt ? a[0] * -1 : a[0];
+         let y = a[1] + offset;
 
-			return [x, y];
-		});
-	}
+         return [x, y];
+      });
+   }
 
-	const createPath = (value: string, lr = false, offset = 0) => {
+   const createPath = (value: string, lr = false, offset = 0) => {
 
-		// Static center bar
-		let path = 'M 0 -1.5 L 0 1.5 ';
+      // Static center bar
+      let path = 'M 0 -1.5 L 0 1.5 ';
 
-		for (let i = 0; i < value.length; i++) {
+      for (let i = 0; i < value.length; i++) {
 
-			// Shift coordinates according to digit position
-			// Digit 1 and 3 are horizontally mirrored
-			let rl = i % 2 !== 0;
+         // Shift coordinates according to digit position
+         // Digit 1 and 3 are horizontally mirrored
+         let rl = i % 2 !== 0;
 
-			// Digit 0 and 1 have negative vertical offset, 2 and 3 positive
-			let offset = (i == 0 || i == 1) ? -1.5 : .5;
+         // Digit 0 and 1 have negative vertical offset, 2 and 3 positive
+         let offset = (i == 0 || i == 1) ? -1.5 : .5;
 
-			let coords = shift(Cistercian[parseInt(value[value.length - i - 1])], rl, offset);
+         let coords = shift(Cistercian[parseInt(value[value.length - i - 1])], rl, offset);
 
-			path += ' M ' + coords[0].join(' ');
-			path += ' L ' + coords.slice(1).map(a => a.join(', '));
-		}
+         path += ' M ' + coords[0].join(' ');
+         path += ' L ' + coords.slice(1).map(a => a.join(', '));
+      }
 
-		return path;
-	}
+      return path;
+   }
 
-	return (
+   return (
 
-		<>
-			<div className="input">
-				<input type="text" name="value" maxLength={4} value={value} onChange={handleChange} />
-			</div>
+      <>
+         <div className="input">
+            <input type="text" name="value" maxLength={4} value={value} onChange={handleChange} />
+         </div>
 
-			<div className="digit">
-				<svg viewBox="-1.1 -1.6 2.2 3.2">
-					<path d={createPath(value)} />
-				</svg>
-			</div>
-		</>
-	)
+         <div className="digit">
+            <svg viewBox="-1.1 -1.6 2.2 3.2">
+               <path d={createPath(value)} />
+            </svg>
+         </div>
+      </>
+   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,46 +3,46 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.ts',
+   mode: 'production',
+   entry: './src/index.ts',
 
-  plugins: [
-    new webpack.ProgressPlugin(), 
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    })],
+   plugins: [
+      new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({
+         template: 'index.html'
+      })],
 
-  module: {
-    rules: [{
-      test: /\.(ts|tsx)$/,
-      loader: 'ts-loader',
-      include: [path.resolve(__dirname, 'src')],
-      exclude: [/node_modules/]
-    }, {
-      test: /.(sa|sc|c)ss$/,
-
-      use: [{
-        loader: "style-loader"
+   module: {
+      rules: [{
+         test: /\.(ts|tsx)$/,
+         loader: 'ts-loader',
+         include: [path.resolve(__dirname, 'src')],
+         exclude: [/node_modules/]
       }, {
-        loader: "css-loader",
-        options: { sourceMap: true }
-      }, 
-      {
-        loader: "sass-loader",
-        options: { sourceMap: true }
+         test: /.(sa|sc|c)ss$/,
+
+         use: [{
+            loader: "style-loader"
+         }, {
+            loader: "css-loader",
+            options: { sourceMap: true }
+         },
+         {
+            loader: "sass-loader",
+            options: { sourceMap: true }
+         }]
       }]
-    }]
-  },
+   },
 
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js']
-  },
+   resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+   },
 
-  devServer: {
-    open: true,
-    host: 'localhost',
-    port: 8084
-  },
+   devServer: {
+      open: true,
+      host: 'localhost',
+      port: 8084
+   },
 
-  devtool: 'source-map'
+   devtool: 'source-map'
 }
